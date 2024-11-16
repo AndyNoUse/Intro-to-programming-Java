140daa9c026887cacb170546324d72504f6ca9ac
AndyNoUse <62490458+AndyNoUse@users.noreply.github.com>
2024-11-16 16:00:42 +0100

    Create WordAndRowCounterTest.java and add 3 testcases.

--- src/WordAndRowCounterTest.java
+++ src/WordAndRowCounterTest.java
@@ -5,20 +5,29 @@ import static org.junit.Assert.assertEquals;
 
 public class WordAndRowCounterTest {
 
-@Test
-    void wordCounter(){
-    WordAndRowCounter WordAndRowCounter = new WordAndRowCounter("Hej" +
-            "jag heter" +
-            "LoveNilsson");
-    assertEquals(4, WordAndRowCounter.wordCount());
-}
     @Test
-    void rowCounter(){
+    void wordCounter() {
         WordAndRowCounter WordAndRowCounter = new WordAndRowCounter("Hej" +
                 "jag heter" +
                 "LoveNilsson");
-        assertEquals(3, WordAndRowCounter.rowCount());
+        assertEquals(4, WordAndRowCounter.wordCount());
     }
 
+    @Test
+    void rowCounter() {
+        WordAndRowCounter WordAndRowCounter = new WordAndRowCounter("Hej" +
+                "jag heter" +
+                "LoveNilsson" +
+                "stop");
+        assertEquals(3, WordAndRowCounter.rowCount());
+    }
+    @Test
+    void charCounter() {
+        WordAndRowCounter WordAndRowCounter = new WordAndRowCounter("Hej" +
+                "jag heter" +
+                "LoveNilsson" +
+                "stop");
+        assertEquals(22, WordAndRowCounter.charCount());
+    }
 
 }
