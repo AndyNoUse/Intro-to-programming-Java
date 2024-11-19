4f1ebafa173ba224af405b9275c5cba9245dfd91
AndyNoUse <62490458+AndyNoUse@users.noreply.github.com>
2024-11-19 22:05:39 +0100

    Create WordAndRowCounterTest.java and add 3 testcases.

---
+++ src/WordAndRowCounter.java
@@ -0,0 +1,11 @@
+public class WordAndRowCounter {
+
+    private String wordArray[];
+
+    public WordAndRowCounter() {
+        this.wordArray = wordArray;
+    }
+
+    public void inputWords(String s) {
+    }
+}
--- src/WordAndRowCounterTest.java
+++ src/WordAndRowCounterTest.java
@@ -4,30 +4,58 @@ import org.junit.jupiter.api.Test;
 import static org.junit.Assert.assertEquals;
 
 public class WordAndRowCounterTest {
+//Skriv ett program som läser in text ifrån kommandoraden rad för rad tills användaren skriver ordet stop.
+//
+//När användaren är klar skriver programmet ut antal tecken och hur många rader som användaren har skrivit, exklusive raden med ordet stop
+    //Programmet skall bestå av två klasser:
+//En klass som läser in text och skriver ut resultatet till användaren
+//En annan klass som räknar raderna och har koll på antalet tecken
+//Kontrollen för att ha koll på om användaren har skrivit ordet stop eller inte får utföras i vilken av klasserna man vill
+//Till programmet skall minst tre Junit-testfall skrivas
+//
+//Testfallen skall skilja sig märkvärt åt och testa olika delar av programmet
+//Skriv antal tecken men notera om det är med eller utan mellanslag
 
-    @Test
-    void wordCounter() {
-        WordAndRowCounter WordAndRowCounter = new WordAndRowCounter("Hej" +
-                "jag heter" +
-                "LoveNilsson");
-        assertEquals(4, WordAndRowCounter.wordCount());
-    }
+    //Del 3
+//
+//Vidareutveckla ditt program i del 2 så att programmet även gör följande:
+//
+//Skriver ut antal ord (separerade med blanksteg)
+//Skriver ut det längsta ordet
+//
+//Kontrollen om användaren har skrivit ordet stop eller inte måste ligga i den andra ”logik-klassen” Programmet skall fortfarande bestå av två klasser:
+//En klass som läser in text och skriver ut resultatet till användaren
+//En annan klass som:
+//Räknar raderna, antal tecken och antal ord
+//Har koll på det längsta ordet
+//Har koll på om användaren har skrivit ordet stop eller inte Programmet skall fortfarande ha minst 3 testfall och läggas upp på samma sätt i Github som i del 2
 
-    @Test
-    void rowCounter() {
-        WordAndRowCounter WordAndRowCounter = new WordAndRowCounter("Hej" +
-                "jag heter" +
-                "LoveNilsson" +
-                "stop");
-        assertEquals(3, WordAndRowCounter.rowCount());
-    }
-    @Test
-    void charCounter() {
-        WordAndRowCounter WordAndRowCounter = new WordAndRowCounter("Hej" +
-                "jag heter" +
-                "LoveNilsson" +
-                "stop");
-        assertEquals(22, WordAndRowCounter.charCount());
-    }
+//    @Test
+//    void addWords() {
+//        WordAndRowCounter wordAndRowCounter = new WordAndRowCounter();
+//        wordAndRowCounter.inputWords("Hej" + "jag heter" + "LoveNilsson");
+//        assertEquals("Hej" + "jag heter" + "LoveNilsson", WordAndRowCounter.getWords());
+//    }
+
+//    @Test
+//    void countWords() {
+//        WordAndRowCounter wordAndRowCounter = new WordAndRowCounter();
+//        wordAndRowCounter.inputWords("Hej" + "jag heter" + "LoveNilsson");
+//        assertEquals(4, WordAndRowCounter.wordCount());
+//    }
+//
+//    @Test
+//    void rowCounter() {
+//        WordAndRowCounter wordAndRowCounter = new WordAndRowCounter();
+//        wordAndRowCounter.inputWords("Hej" + "jag heter" + "LoveNilsson");
+//        assertEquals(3, WordAndRowCounter.rowCount());
+//    }
+//
+//    @Test
+//    void charCounter() {
+//        WordAndRowCounter wordAndRowCounter = new WordAndRowCounter();
+//        wordAndRowCounter.inputWords("Hej" + "jag heter" + "LoveNilsson");
+//        assertEquals(22, WordAndRowCounter.charCount());
+//    }
 
 }
